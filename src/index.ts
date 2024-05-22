53692fd8cfd970f2f9ec08bcc077d9d1c588d839
radumir0n <radumiron12@gmail.com>
2024-05-22 18:21:52 +0300

    --chore: added base express app + lint setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('test');
+import express, { Request, Response } from "express"
+import morgan from "morgan"
+
+const app = express()
+app.use(morgan("dev"))
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ "hello": "world" })
+})
+
+const port = Number(process.env.PORT || 5000)
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server listening on port ${port}`)
+})
